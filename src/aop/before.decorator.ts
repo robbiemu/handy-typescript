2d6a1a1a9d1d03074038bd0582c94c30882fd314
Roberto Tomás Collins <roberto.tomas.cuentas@gmail.com>
2021-05-22 12:09:34 -0400

    lint-staged-2

--- src/aop/before.decorator.ts
+++ src/aop/before.decorator.ts
@@ -9,17 +9,17 @@ export function Before(beforeFunction: Function) {
     _propertyKey: string | symbol,
     descriptor: PropertyDescriptor,
   ) {
-    const originalMethod = descriptor.value
+    const originalMethod = descriptor.value;
 
     descriptor.value = function (...args: any[]) {
       function res() {
         beforeFunction.bind(this)(...args)
         const result = originalMethod.bind(this)(...args)
 
-        return result;
+        return result
       }
 
-      return res.bind(this)();
+      return res.bind(this)()
     }
 
     return descriptor
